56192560c39c5a0ae35ad5758d983d91ec913a16
ryan <4721319+rpshukla@users.noreply.github.com>
2019-10-12 00:46:12 -0600

    Implement piecewise non-linear function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
+#include <cmath>
 #include <unistd.h> // sleep()
 
 #include <jack_wrapper/jack_wrapper.h>
 
 jack_port_t *inputPort;
 jack_port_t *outputPort;
+
+jack_default_audio_sample_t distortionFunction(jack_default_audio_sample_t x) {
+  if (x < -0.08905) {
+    // Assume x >= -1
+    // Therefore, this first interval is actually -1 <= x < -0.08905
+    return -(3 / 4) * (1 - pow((1 - (-x - 0.032847)), 12) +
+                       (1 / 3) * (-x - 0.032847)) +
+           0.01;
+  } else if (x < 0.320018) {
+    return -6.153 * pow(x, 2) + 3.9375 * x;
+  } else {
+    // Assume x <= 1
+    // Therefore, this last interval is actually 0.320018 <= x <= 1
+    return 0.630035;
+  }
+}
 
 /**
  * Callback to process audio.
@@ -14,10 +31,7 @@ int processCallback(jack_nframes_t nframes, void *arg) {
   out =
       (jack_default_audio_sample_t *)jack_port_get_buffer(outputPort, nframes);
   for (size_t i = 0; i < nframes; ++i) {
-    jack_default_audio_sample_t tmp = in[i];
-    // Creates nonlinear distortion
-    tmp = 3 * tmp * tmp;
-    out[i] = tmp;
+    out[i] = distortionFunction(in[i]);
   }
 
   return 0;
